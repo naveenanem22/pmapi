848da85c3fd7ee0e509ca9e35c86c286ad4de0fb
Naveen Anem <naveenanem22@gmail.com>
2018-05-20 18:03:12 +1000

    Adding exception handling..in progress

---
+++ src/main/java/com/pmt/common/PMAPIConstants.java
@@ -0,0 +1,7 @@
+package com.pmt.common;
+
+public class PMAPIConstants {
+	public static String REST_STATUS_SUCCESS = "Success";
+	public static String REST_STATUS_FAILURE = "Service Failure";
+
+}
--- src/main/java/com/pmt/controller/RestController.java
+++ src/main/java/com/pmt/controller/RestController.java
@@ -1,14 +1,6 @@
 package com.pmt.controller;
 
-import java.util.ArrayList;
 import java.util.List;
-
-
-
-
-
-
-
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -24,30 +16,26 @@ import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.method.annotation.RequestHeaderMapMethodArgumentResolver;
-import org.springframework.web.servlet.ModelAndView;
 
 
-
-
-
-
-
-
-
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.pmt.model.Skill;
 import com.pmt.service.SkillService;
-import com.pmt.util.response.SimpleResult;
+import com.pmt.util.response.ResultWithData;
+import com.pmt.util.response.ResultWithData;
 
+import static com.pmt.common.PMAPIConstants.*;
+
+/**
+* This is the main controller handling all the request and response
+* for various APIs across the application. 
+* 
+* @author  Naveen Anem
+* @version 1.0
+* @since   2017-01-01 
+*/
 @Controller
 @Path("/projmgmt/V1/")
 public class RestController {
@@ -61,26 +49,24 @@ public class RestController {
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	@Path("/healthcheck")
-	public Response healthCheck(){		
-		SimpleResult result = new SimpleResult();
-		result.setStatus("Success");
-		GenericEntity<SimpleResult> entity = new GenericEntity<SimpleResult>(result){};
+	public Response healthCheck() {
+		
+		ResultWithData result = new ResultWithData();
+		result.setStatus(REST_STATUS_SUCCESS);
+		GenericEntity<ResultWithData> entity = new GenericEntity<ResultWithData>(result){};
 		return Response.status(Status.OK).entity(entity).build();
 	}
 
 	@GET
 	@Path("/skills")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response listSkills() {
-		logger.info("Getting skills...");
-		//Skill skill = new Skill();
-		//skill.setId(23);
-		//skill.setCategory("domain");
-		//skill.setDescription("Payments");
-		//skill.setName("CuscalApi");
-		//skillService.addSkill(skill);		
+	public Response listSkills() {		
+		ResultWithData result = new ResultWithData();
+		
 		List<Skill> skills =  skillService.listSkills();
-		GenericEntity<List<Skill>> entity = new GenericEntity<List<Skill>>(skills){};		
+		result.setStatus(REST_STATUS_SUCCESS);
+		result.setData(skills);
+		GenericEntity<ResultWithData> entity = new GenericEntity<ResultWithData>(result){};		
 		return Response.ok(entity).build();
 	}
 	
@@ -89,17 +75,17 @@ public class RestController {
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response addSkill(Skill skill){		
-		SimpleResult result = new SimpleResult();
+		ResultWithData result = new ResultWithData();
 		
 		try{
 		skillService.addSkill(skill);
-		result.setStatus("Success");
+		result.setStatus(REST_STATUS_SUCCESS);
 		}catch(Exception ex){
-			result.setStatus("Failure");
+			result.setStatus(REST_STATUS_FAILURE);
 			return Response.status(Status.INTERNAL_SERVER_ERROR).entity(result).build();
 		}
 				
-		GenericEntity<SimpleResult> entity = new GenericEntity<SimpleResult>(result){};
+		GenericEntity<ResultWithData> entity = new GenericEntity<ResultWithData>(result){};
 		return Response.ok(entity).build();
 	}
 	
@@ -107,11 +93,19 @@ public class RestController {
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Path("/testrespbld")
-	public Response testResponseBuilder(){
-		SimpleResult result = new SimpleResult();
-		result.setStatus("Failure at server");
-		GenericEntity<SimpleResult> entity = new GenericEntity<SimpleResult>(result){};
-		return Response.status(Status.INTERNAL_SERVER_ERROR).entity(result).build();
+	public Response testResponseBuilder(){		
+		ResultWithData result = new ResultWithData();		
+		result.setStatus(REST_STATUS_FAILURE);
+		GenericEntity<ResultWithData> entity = new GenericEntity<ResultWithData>(result){};
+		return Response.status(Status.INTERNAL_SERVER_ERROR).entity(entity).build();
+	}
+	
+	@ExceptionHandler(Exception.class)
+	public Response localExceptionHandler(){
+		ResultWithData result = new ResultWithData();		
+		result.setStatus(REST_STATUS_FAILURE);
+		GenericEntity<ResultWithData> entity = new GenericEntity<ResultWithData>(result){};
+		return Response.status(Status.INTERNAL_SERVER_ERROR).entity(entity).build();
 	}
 	
 
