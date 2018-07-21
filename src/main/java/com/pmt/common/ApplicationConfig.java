3bc431cc844c677642b0d1f4117ea29ffb3d7165
Naveen Anem <naveenanem22@gmail.com>
2018-07-22 08:14:15 +1000

    Initial Draft of Javabased application config

---
+++ src/main/java/com/pmt/common/ApplicationConfig.java
@@ -0,0 +1,21 @@
+package com.pmt.common;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@ComponentScan(basePackages = "com.pmt")
+@Configuration
+public class ApplicationConfig {
+	
+	@Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        jdbcTemplate.setResultsMapCaseInsensitive(true);
+        return jdbcTemplate;
+    }
+
+}
